9de4beaa6353f91363bcd50de93414bd2d4e0c76
Boya Du <BoyaDu@Boyas-Air.attlocal.net>
2016-12-20 13:44:02 -0600

    Modify entertainment_center.py and media.py based on pep8 coding style and push the html result in.

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,29 +2,32 @@ import media
 import fresh_tomatoes
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys that come to life",
-                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",  # NOQA
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
-#print(toy_story.storyline)
+# print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # NOQA
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-#avatar.show_trailer()
+# avatar.show_trailer()
 school_of_rock = media.Movie("School of Rock", "Using rock music to learn",
-                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",  # NOQA
                              "https://www.youtube.com/watch?v=3PsUJFEBC74")
 ratatouille = media.Movie("Ratatouille", "A rat is a chef in Paris",
-                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                          "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",  # NOQA
                           "https://www.youtube.com/watch?v=uVeNEbh3A4U&t=5s")
-midnight_in_paris = media.Movie("Midnight in Paris", "Going back in time to meet authors",
-                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "Going back in time to meet authors",
+                                "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",  # NOQA
                                 "https://www.youtube.com/watch?v=FAfR8omt-CY")
-the_hunger_games = media.Movie("The Hunger Games", "A really real reality show",
-                           "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
-                           "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+the_hunger_games = media.Movie("The Hunger Games",
+                               "A really real reality show",
+                               "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",  # NOQA
+                               "https://www.youtube.com/watch?v=4S9a5V9ODuY")
 
-movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_paris, the_hunger_games]
+movies = [toy_story, avatar, school_of_rock,
+          ratatouille, midnight_in_paris, the_hunger_games]
 fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.__module__)
+# print(media.Movie.__module__)
